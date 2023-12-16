b7bbb4081b5826056b2779b0521f180f635ee42f
Dave Chang <juzen2003@gmail.com>
2023-12-15 17:29:39 -0800

    Add pdscache.py to pdsfile module.

--- pdsfile/pds3file/__init__.py
+++ pdsfile/pds3file/__init__.py
@@ -4,8 +4,8 @@
 ##########################################################################################
 
 import pdslogger
-import pdscache
 
+from pdsfile import pdscache
 from pdsfile.pdsfile import PdsFile
 from . import rules
 from pdsfile.preload_and_cache import cache_lifetime_for_class
