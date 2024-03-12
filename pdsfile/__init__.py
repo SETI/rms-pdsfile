01bf2d97385f6bd64dbee75e24121aa543767cad
Robert French <rfrench@seti.org>
2024-03-12 14:59:42 -0700

    Add version info (#29)

--- pdsfile/__init__.py
+++ pdsfile/__init__.py
@@ -2,6 +2,11 @@
 # pdsfile/__init__.py
 ##########################################################################################
 
+try:
+    from ._version import __version__
+except ImportError as err:
+    __version__ = 'Version unspecified'
+
 from pdsfile import *
 from .pds3file import *
 from .pds4file import *
