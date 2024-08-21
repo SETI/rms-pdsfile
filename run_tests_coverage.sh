66fce1b0cfdc9b53b3f38e9f60526c8ced91f445
Dave Chang <juzen2003@gmail.com>
2024-08-21 16:05:38 -0700

    Update run_tests_coverage.sh to take in '--update' param. When '--udpate' is passed in, the script will update the opus products golden copies.

--- run_tests_coverage.sh
+++ run_tests_coverage.sh
@@ -3,17 +3,24 @@ echo "Clean up previous coverage record"
 coverage erase
 if [ $? -ne 0 ]; then exit -1; fi
 
+# If --udpate is given, we will update the opus products golden copies
+if [  -n "$1"  ] && [ "$1" != "--update" ]
+then
+    echo "Exiting the program. Only options: None or '--update'"
+    exit -1
+fi
+
 echo "Run with use shelves on PDS3"
 coverage run --parallel-mode -m pytest pdsfile/pds3file/tests/ \
-    pdsfile/pds3file/rules/*.py --mode s
+    pdsfile/pds3file/rules/*.py --mode s $1
 if [ $? -ne 0 ]; then exit -1; fi
 echo "Run with no shelves on PDS3"
 coverage run --parallel-mode -m pytest pdsfile/pds3file/tests/ \
-    pdsfile/pds3file/rules/*.py --mode ns
+    pdsfile/pds3file/rules/*.py --mode ns $1
 if [ $? -ne 0 ]; then exit -1; fi
 echo "Run with no shelves on PDS4"
 coverage run --parallel-mode -m pytest pdsfile/pds4file/tests/ \
-    pdsfile/pds4file/rules/*.py --mode ns
+    pdsfile/pds4file/rules/*.py --mode ns $1
 if [ $? -ne 0 ]; then exit -1; fi
 
 echo "Combine results from all modes"
