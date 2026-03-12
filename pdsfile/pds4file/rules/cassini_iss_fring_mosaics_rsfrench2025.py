0d3280ed28e82db496ef7966729b880e0bb8a352
Dave Chang <juzen2003@gmail.com>
2026-03-12 00:31:16 -0700

    Fixed indentation in opus_products rules in cassini_iss_fring_mosaics_rsfrench2025.py

--- pdsfile/pds4file/rules/cassini_iss_fring_mosaics_rsfrench2025.py
+++ pdsfile/pds4file/rules/cassini_iss_fring_mosaics_rsfrench2025.py
@@ -169,7 +169,7 @@ opus_type = translator.TranslatorByRegex([
 
 opus_products = translator.TranslatorByRegex([
     (r'bundles/(cassini_iss_fring_mosaics_rsfrench2025/cassini_iss_fring_mosaics_rsfrench2025[^/]*)/(data|browse)_mosaic.*/((iss|iosic)_[a-zA-Z0-9_]*)/(iss|iosic)_[a-zA-Z0-9_]*_mosaic.*\.[a-z]{3,4}', 0,
-    [
+     [
         # bundles data_mosaic/
         r'bundles/\1/data_mosaic/\3/\3_mosaic.img',
         r'bundles/\1/data_mosaic/\3/\3_mosaic.lblx',
@@ -203,7 +203,7 @@ opus_products = translator.TranslatorByRegex([
         r'bundles/\1/browse_mosaic_bkg_sub/\3/\3_browse_mosaic_bkg_sub_med.png',
         r'bundles/\1/browse_mosaic_bkg_sub/\3/\3_browse_mosaic_bkg_sub_small.png',
         r'bundles/\1/browse_mosaic_bkg_sub/\3/\3_browse_mosaic_bkg_sub_thumb.png',
-    ]),
+     ]),
 ])
 
 ##########################################################################################
